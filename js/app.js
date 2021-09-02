91f351c77853ea2e003add4de17fcec45b09c6dc
iazadur <iamazadur@gmail.com>
2021-09-02 09:15:31 +0600

    all update

--- js/app.js
+++ js/app.js
@@ -7,11 +7,28 @@ const error = document.getElementById('error')
 const searchResult = document.getElementById('searchResult')
 const showingResult = document.getElementById('showingResult')
 
+// control spinner 
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle
+}
+
+// check undefined property in Api 
+const checkUndefined = data => {
+    if (data === undefined) {
+        return "No Result Found"
+    } else {
+        return data
+    }
+}
+
 
 // get data from api 
 const loadData = () => {
-    const url = `http://openlibrary.org/search.json?q=${searchText.value}`
+    const url = `https://openlibrary.org/search.json?q=${searchText.value}`
     searchText.value = ''
+    books.innerHTML = ''
+    searchResult.innerText = ''
+    showingResult.innerText = ' '
     error.style.display = 'none'
     fetch(url)
         .then(res => res.json())
@@ -24,12 +41,11 @@ const loadData = () => {
 // display book like search keyword
 const displayData = data => {
 
-
-
-    searchResult.innerText = `Search Result: ${data.numFound}`
+    searchResult.innerText = `${data.numFound} Result Found`
 
     if (data.numFound === 0) {
         toggleSpinner('none')
+        searchResult.innerText = 'No Result Found'
         error.style.display = 'block'
         error.innerText = 'Oops something went wrong. Please Enter a valid input'
     } else {
@@ -44,9 +60,18 @@ const displayData = data => {
                     <div class="card h-100">
                         <img src="${coverPhoto}" class="card-img-top" alt="">
                         <div class="card-body">
-                        <h5 class="card-title">Book Name: ${element.title}</h5>
-                        <h6 class="card-title">Aouthor Name: ${element.author_name}</h6>
-                        <h6 class="card-title">First Published Date: ${element.first_publish_year}</h6> 
+                        <h5 class="card-title">
+                        <span class="fw-bolder fs-5">Book Name: </span>
+                        <span class="fw-normal fs-5">${checkUndefined(element.title)}</span>
+                        </h5>
+                        <h6 class="card-title">
+                        <span class="fw-bolder fs-6">Aouthor Name: </span>
+                        <span class="fw-normal fs-6">${checkUndefined(element.author_name)}</span>
+                        </h6>
+                        <h6 class="card-title">
+                        
+                        <span class="fw-bolder fs-6">First Published Year: </span>
+                        <span class="fw-normal fs-6">${checkUndefined(element.first_publish_year)}</span></h6>
                         </div>
                     </div>
         
@@ -58,8 +83,4 @@ const displayData = data => {
         showingResult.innerText = `Showing Result: ${num}`
     }
 
-}
-
-const toggleSpinner = displayStyle => {
-    document.getElementById('spinner').style.display = displayStyle
 }
